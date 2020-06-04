95305ed71c37f8961b56948a4d79812979dff543
bita-alexandru <36082814+bita-alexandru@users.noreply.github.com>
2020-06-05 01:12:24 +0300

    Add files via upload

--- Backend/controllers/server.js
+++ Backend/controllers/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const userController = require('./user');
+const viewController = require('./view');
 
 let server = http.createServer((request, response) => {
     let parsedUrl = url.parse(request.url, true);
@@ -31,14 +32,25 @@ let server = http.createServer((request, response) => {
             'payload': buffer
         };
 
-        handler(data, response);
+        let file = __dirname + `/../views/${trimmedPath}`;
 
-        // console.log(`${method}: ${trimmedPath}`);
+        if (trimmedPath.startsWith('assets/')) {
+            if (trimmedPath.endsWith('.css')) {
+                viewController.getCSS(file, response, 200);
+            }
+            else if (trimmedPath.endsWith('.js')) {
+                viewController.getJS(file, response, 200);
+            } else if (trimmedPath.endsWith('.ico')) {
+                //viewController.getICO(file, response, 200);
+            }
+        } else {
+            handler(data, response);
+        }
+
+        console.log(`${method}: ${trimmedPath}`);
         // console.log('query: ' + JSON.stringify(queryString, null, " "));
         // console.log('headers: ' + JSON.stringify(headers, null, " "));
         // console.log('payload: ' + buffer);
-
-        response.end();
     });
 
 });
@@ -47,15 +59,14 @@ const router = {
     'register': userController.register,
     'login': userController.login,
     'logout': userController.logout,
+    'account': userController.account,
     'delete_account': userController.deleteAccount,
     '': userController.feed,
     'preferences': userController.preferences,
     'not_found': function notFound(data, response) {
-        let payload = data.payload;
-       // let values = JSON.parse(payload);
-       // console.log(values['aaaaaaaa']);
-        response.writeHead(404);
-        response.write('Not found');
+        let file = __dirname + '/../views/not_found.html';
+
+        viewController.getHTML(file, response, 404);
     }
 };
 
--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -1,27 +1,39 @@
 const http = require('http');
+const viewController = require('./view');
+const user = require('../models/user');
 
 function register(data, response) {
-
+    response.end();
 }
 
 function login(data, response) {
-
+    response.end();
 }
 
 function logout(data, response) {
+    response.end();
+}
 
+function account(data, response) {
+    let file = __dirname + '/../views/account.html';
+
+    viewController.getHTML(file, response, 200);
 }
 
 function deleteAccount(data, response) {
-
+    response.end();
 }
 
 function feed(data, response) {
+    let file = __dirname + '/../views/feed.html';
 
+    viewController.getHTML(file, response, 200);
 }
 
 function preferences(data, response) {
+    let file = __dirname + '/../views/preferences.html';
 
+    viewController.getHTML(file, response, 200);
 }
 
-module.exports = { register, login, logout, deleteAccount, feed, preferences };
+module.exports = { register, login, logout, account, deleteAccount, feed, preferences };
