592bd5f3937c2d75080f6833d6f76311d854784c
Danu Teodor <danu.teo20@gmail.com>
2020-06-06 21:19:05 +0300

    implement get prefs

---
+++ Backend/controllers/default_preferences.js
@@ -0,0 +1,25 @@
+function domains() {
+    const domains = [
+        'Technology',
+        'Politics',
+        'Sports',
+        'Economy',
+        'Animals',
+        'Music',
+        'Video'
+    ];
+    return domains;
+}
+
+function websites() {
+    const websites = [
+        'www.bbc.com',
+        'www.digi24.ro',
+        'www.youtube.com',
+        'www.spotify.com',
+        'www.reddit.com'
+    ];
+    return websites;
+}
+
+module.exports = { domains, websites };
