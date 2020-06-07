08ffb9118429bd53c0ca1f454bc416167d73941d
Danu Teodor <danu.teo20@gmail.com>
2020-06-07 21:56:07 +0300

    Administrate and export users

--- Backend/util/admin.js
+++ Backend/util/admin.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const userModel = require('../models/user');
 const resourceModel = require('../models/resource');
 const responder = require('./responder');
+const preferences = require('../util/available_preferences')
+
 
 let usables = {
     usableFeed: true,
@@ -187,15 +190,207 @@ function isAdmin(data, callback) {
 }
 
 function exportUsers(data, response) {
+    isAdmin(data, result => {
+        if (result === 200) {
+            if (data.method === 'GET') {
+                userModel.find( // get all user
+                    null,
+                    { password: 0 },
+                    (err, resources) => {
+                        if (err) { // something went wrong, perhaps an internal error
+                            responder.status(response, 500);
+                        } else {
+                            const date = new Date();
+                            const file = 'User_' + date.getFullYear() + '_' + (date.getMonth() + 1) + '_' + date.getDate() + '.json';
 
+                            fs.writeFile(
+                                file,
+                                JSON.stringify(resources, null, " "), err => {
+                                    if (err) {
+                                        responder.status(response, 500);
+                                    } else {
+                                        responder.status(response, 200);
+                                    }
+                                });
+                        }
+                    }
+                );
+            } else {
+                responder.status(response, 400);
+            }
+        } else {
+            responder.status(response, result);
+        }
+    });
 }
 
 function exportResources(data, response) {
+    isAdmin(data, result => {
+        if (result === 200) {
+            if (data.method === 'GET') {
+                resourceModel.find( // get all resources
+                    (err, resources) => {
+                        if (err) { // something went wrong, perhaps an internal error
+                            responder.status(response, 500);
+                        } else {
+                            const date = new Date();
+                            const file = 'Resources_' + date.getFullYear() + '_' + (date.getMonth() + 1) + '_' + date.getDate() + '.json';
 
+                            fs.writeFile(
+                                file,
+                                JSON.stringify(resources, null, " "), err => {
+                                    if (err) {
+                                        responder.status(response, 500);
+                                    } else {
+                                        responder.status(response, 200);
+                                    }
+                                });
+                        }
+                    }
+                );
+            } else {
+                responder.status(response, 400);
+            }
+        } else {
+            responder.status(response, result);
+        }
+    });
 }
 
 function manageUser(data, response) {
+    // isAdmin(data, result => {
+        // if (result === 200) {
+            if (data.method === 'POST') {
+                try {
+                    const values = JSON.parse(data.payload);
+                    const password = values.password;
+                    const saltRounds = 13;
 
+                    bcrypt.hash(password, saltRounds, (err, hash) => { // encrypt the password
+                        if (err) {
+                            responder.status(response, 500); // something went wrong, perhaps an internal error
+                        } else {
+                            userModel.create( // create and store a new user
+                                {
+                                    _id: mongoose.Types.ObjectId(),
+                                    username: values.username,
+                                    password: hash,
+                                    preferredDomains: preferences.default_domains,
+                                    preferredSites: preferences.default_websites
+                                },
+                                err => {
+                                    if (err) {
+                                        responder.status(response, 500);
+                                    } else {
+                                        responder.status(response, 200);
+                                    }
+                                }
+                            );
+                        }
+                    });
+                } catch {
+                    responder.status(response, 400);
+                }
+            } else if (data.method === 'GET') {
+                try {
+                    const values = JSON.parse(data.payload);
+                    const username = values.username;
+
+                    userModel.find({ username: username }, 'username', (err, user) => {
+                        if (err) {
+                            responder.status(response, 500);
+                        }
+                        else {
+                            if (user) {
+                                userModel.find(
+                                    { username: username },
+                                    { password: 0 },
+                                    (err, resources) => {
+                                        if (err) { // something went wrong, perhaps an internal error
+                                            responder.status(response, 500);
+                                        } else {
+                                            const date = new Date();
+                                            const file = 'User_' + date.getFullYear() + '_' + (date.getMonth() + 1) + '_' + date.getDate() + '.json';
+
+                                            fs.writeFile(
+                                                file,
+                                                JSON.stringify(resources, null, " "), err => {
+                                                    if (err) {
+                                                        responder.status(response, 500);
+                                                    } else {
+                                                        responder.status(response, 200);
+                                                    }
+                                                });
+                                        }
+                                    }
+                                );
+                            } else {
+                                responder.status(response, 401);
+                            }
+                        }
+                    });
+
+                } catch{
+                    responder.status(response, 400)
+                }
+            } else if (data.method === 'DELETE') {
+                try {
+                    const values = JSON.parse(data.payload);
+                    const username = values.username;
+
+                    userModel.find({ username: username }, 'username', (err, user) => {
+                        if (err) {
+                            responder.status(response, 500);
+                        } else {
+                            if (user) {
+                                userModel.deleteOne({ username: username }, (err) => {
+                                    if (err) {
+                                        responder.status(response, 500);
+                                    } else {
+                                        responder.status(response, 200);
+                                    }
+                                });
+                            } else {
+                                responder.status(response, 500);
+                            }
+                        }
+                    });
+                } catch{
+                    responder.status(response, 400);
+                }
+            } else if (data.method === 'PUT') {
+                try {
+                    const values = JSON.parse(data.payload);
+                    const username = values.username;
+
+                    userModel.findOne({ username: username }, 'username preferredDomains preferredSites', (err, user) => {
+                        if (err) {
+                            responder.status(response, 500);
+                        } else {
+                            if (user) {
+                                if (values.preferredDomains !== 'undefined') {
+                                    user.preferredDomains = values.preferredDomains;
+                                }
+                                if (values.preferredSites !== 'undefined') {
+                                    user.preferredSites = values.preferredSites;
+                                }
+                                user.save();
+                                responder.status(response, 200);
+                            }
+                            else {
+                                responder.status(response, 500);
+                            }
+                        }
+                    });
+                    responder.status(response, 200);
+                } catch{
+                    responder.status(response, 400);
+                }
+            }
+        // } else {
+            // responder.status(response, result);
+        // }
+    // })x  ;
 }
 
 function manageResource(data, response) {
@@ -224,11 +419,9 @@ function manageResource(data, response) {
                         }
                     );
                 } catch {
-                    console.log('invalid json')
                     responder.status(response, 400);
                 }
             } else {
-                console.log('not post')
                 responder.status(response, 400);
             }
         } else {
